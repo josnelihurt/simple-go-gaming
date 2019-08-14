f6149a65c28115201a8b19e50b6ffbd35116ea54
Josnelihurt R <josnelihurt@gmial.com>
2019-08-14 17:57:22 -0400

    New code

---
+++ bullet_mover.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+type bulletMover struct {
+	parent *element
+	speed  float64
+}
+
+func newBulletMover(parent *element, speed float64) *bulletMover {
+	return &bulletMover{
+		parent: parent,
+		speed:  speed,
+	}
+}
+
+func (context *bulletMover) onDraw(renderer *sdl.Renderer) error {
+	return nil
+}
+func (context *bulletMover) onUpdate() error {
+	parent := context.parent
+	parent.position.x += bulletSpeed * math.Cos(parent.rotation)
+	parent.position.y += bulletSpeed * math.Sin(parent.rotation)
+
+	if parent.position.x > screenWidth || parent.position.x < 0 ||
+		parent.position.y < 0 {
+		parent.active = false
+	}
+	return nil
+}
--- player.go
+++ player.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
@@ -9,60 +8,27 @@ import (
 
 const (
 	playerSpeed        = 2
-	playerSize         = 105
 	playerShotCooldown = time.Millisecond * 150
+	playerScale        = 0.6
 )
 
-type player struct {
-	texture  *sdl.Texture
-	x, y     float64
-	lastShot time.Time
-}
+func newPlayer(renderer *sdl.Renderer) *element {
+	player := &element{}
+	player.active = true
+	player.rotation = 180
 
-func newPlayer(renderer *sdl.Renderer) (p player, err error) {
-	p.texture = textureFromBMP(renderer, "sprites/player.bmp")
-	p.x = screenWidth / 2
-	p.y = screenHeight - playerSize
+	currentSpriteRenderer := newSpriteRenderer(player, renderer, "sprites/player.bmp", playerScale)
+	player.position = vector{
+		x: screenWidth / 2.0,
+		y: screenHeight - 100, //currentSpriteRenderer.scaledHeight/2.0,
+	}
+	player.addCompoenent(currentSpriteRenderer)
 
-	return p, nil
-}
+	mover := newKeyboardMover(player, playerSpeed)
+	player.addCompoenent(mover)
 
-func (p *player) draw(renderer *sdl.Renderer) {
-	x := p.x - playerSize/2.0
-	y := p.y - playerSize/2.0
-	renderer.Copy(p.texture,
-		&sdl.Rect{X: 0, Y: 0, W: 200, H: 200},
-		&sdl.Rect{X: int32(x), Y: int32(y), W: playerSize, H: playerSize})
-}
-func (p *player) update() {
-	keys := sdl.GetKeyboardState()
+	shooter := newKeyboardShooter(player, playerShotCooldown)
+	player.addCompoenent(shooter)
 
-	if keys[sdl.SCANCODE_LEFT] == 1 {
-		if p.x-(playerSize/2.0) > 0 {
-			p.x = p.x - playerSpeed
-		}
-	}
-	if keys[sdl.SCANCODE_RIGHT] == 1 {
-		if p.x+(playerSize/2.0) < screenWidth {
-			p.x = p.x + playerSpeed
-		}
-	}
-	if keys[sdl.SCANCODE_SPACE] == 1 {
-		if time.Since(p.lastShot) > playerShotCooldown {
-			p.shoot(p.x+25, p.y-40)
-			p.shoot(p.x-25, p.y-40)
-			p.lastShot = time.Now()
-		}
-	}
-}
-func (p *player) shoot(x, y float64) {
-	if b, ok := bulletFromPool(); ok {
-		b.active = true
-		b.x = x
-		b.y = y
-		b.angle = 270 * (math.Pi / 180)
-	}
-}
-func (p *player) destroy() {
-	p.texture.Destroy()
+	return player
 }
