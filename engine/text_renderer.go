87b54f4dcc12b52fcadfbe34bd08f9dce06d902e
josnelihurt <josnelihurt@gmail.com>
2019-08-22 17:53:59 -0500

    adding simple gameover screen

--- engine/text_renderer.go
+++ engine/text_renderer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/veandco/go-sdl2/ttf"
 )
 
+//TextRenderer shows a new text on screen
 type TextRenderer struct {
 	currenValue string
 	newValue    string
@@ -19,6 +20,7 @@ type TextRenderer struct {
 	textColor   sdl.Color
 }
 
+//NewTextRenderer creates a new instance of TextRenderer
 func NewTextRenderer(position *Vector, fontSize int, textColor sdl.Color) *TextRenderer {
 	font, err := util.LoadFont(fontSize)
 	if err != nil {
@@ -32,27 +34,44 @@ func NewTextRenderer(position *Vector, fontSize int, textColor sdl.Color) *TextR
 		position:  position,
 	}
 }
+
+// GetCurrentRenderInfo obtains information about the internal text rendering
+func (context *TextRenderer) GetCurrentRenderInfo() (x, y, width, height float64) {
+	return (context.position.X), (context.position.Y), float64(context.width), float64(context.height)
+}
+
+//SetNewText update the internal text value
 func (context *TextRenderer) SetNewText(text string) {
 	context.newValue = text
 }
+
+//RenderNewValue update the internal surface, this operation could be expensive,
+// and this is called automatiacally if the internal text is not the same as the text represented on screen (OnDraw)
+func (context *TextRenderer) RenderNewValue(renderer *sdl.Renderer) error {
+	surface, err := context.font.RenderUTF8Solid(context.newValue, context.textColor)
+	if err != nil {
+		return fmt.Errorf("creating surface from font %v", err)
+	}
+
+	context.texture, err = renderer.CreateTextureFromSurface(surface)
+	if err != nil {
+		return fmt.Errorf("creating texture from surface %v", err)
+	}
+
+	_, _, context.width, context.height, err = context.texture.Query()
+	if err != nil {
+		return fmt.Errorf("querying texture:%v", err)
+	}
+
+	context.currenValue = context.newValue
+	return nil
+}
+
 func (context *TextRenderer) OnDraw(renderer *sdl.Renderer) error {
 	if context.currenValue != context.newValue {
-		surface, err := context.font.RenderUTF8Solid(context.newValue, context.textColor)
-		if err != nil {
-			return fmt.Errorf("creating surface from font %v", err)
+		if err := context.RenderNewValue(renderer); err != nil {
+			return err
 		}
-
-		context.texture, err = renderer.CreateTextureFromSurface(surface)
-		if err != nil {
-			return fmt.Errorf("creating texture from surface %v", err)
-		}
-
-		_, _, context.width, context.height, err = context.texture.Query()
-		if err != nil {
-			return fmt.Errorf("querying texture:%v", err)
-		}
-
-		context.currenValue = context.newValue
 	}
 	x := context.position.X
 	y := context.position.Y
@@ -62,12 +81,6 @@ func (context *TextRenderer) OnDraw(renderer *sdl.Renderer) error {
 		&sdl.Rect{X: int32(x), Y: int32(y), W: int32(context.width), H: int32(context.height)})
 	return nil
 }
-func (context *TextRenderer) OnUpdate() error {
-	return nil
-}
-func (context *TextRenderer) OnCollision(other *Element) error {
-	return nil
-}
-func (context *TextRenderer) OnMessage(message *Message) error {
-	return nil
-}
+func (context *TextRenderer) OnUpdate() error                  { return nil }
+func (context *TextRenderer) OnCollision(other *Element) error { return nil }
+func (context *TextRenderer) OnMessage(message *Message) error { return nil }
---
+++ screen_game_over.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/josnelihurt/simple-go-gaming/engine"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func newGameOverScreen(components *engine.SDLComponents) *engine.Element {
+	context := &engine.Element{
+		Active: false,
+		Tag:    "GameOverScreen",
+	}
+	textPosition := &engine.Vector{X: screenWidth / 2, Y: screenHeight / 2}
+	textRenderer := engine.NewTextRenderer(textPosition, 40, sdl.Color{R: 0, G: 255, B: 0})
+	textRenderer.SetNewText("Game over !!")
+	textRenderer.RenderNewValue(components.Renderer)
+	_, _, width, _ := textRenderer.GetCurrentRenderInfo()
+	textPosition.X -= width / 2
+	context.AddComponent(textRenderer)
+	return context
+}
